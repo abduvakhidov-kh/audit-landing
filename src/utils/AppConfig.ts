a069ceb50d0c78347bfd5e606c3c8795abb35812
Khabibullo Abduvakhidov <habibulloadbuvahidov03@gmail.com>
2022-12-08 21:49:19 +0500

    modal changes

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
   site_name: 'Audit ava buxgalteriya uslugalari',
   title: 'Audit uz - biz bilan buxgalterga aylaning',
-  description: 'Audit uz - biz bilan haydovchilig guvohnomasini oling',
+  description:
+    'Audit uz - biz bilan buxgalteriya va audit profesialarni ish boshlang',
   locale: 'uz',
 };
