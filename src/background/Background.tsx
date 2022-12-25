073307f4091e2310aaeb98a14b178e082f289303
Khabibullo Abduvakhidov <habibulloadbuvahidov03@gmail.com>
2022-12-26 03:33:38 +0500

    background image fix on deployment

--- src/background/Background.tsx
+++ src/background/Background.tsx
@@ -13,7 +13,11 @@ const Background = (props: IBackgroundProps) => {
   const router = useRouter();
 
   return (
-    <div className={`${props.color} img-bg h-full my-auto pt-5`}>
+    <div
+      className={`${props.color} img-bg h-full my-auto pt-5 bg-[url('${
+        router.basePath + props.img
+      }')]`}
+    >
       {props.children}
       <style jsx>{`
         .img-bg {
